40ad0133165a253089f7b1dd7d5ab448502d5c16
Leticia Magrin <leticiamagrin@outlook.com>
2024-10-26 14:24:22 -0300

    arruma slide

--- src/components/header/body/BlocoAnimatics.js
+++ src/components/header/body/BlocoAnimatics.js
@@ -22,11 +22,11 @@ function BlocoAnimatics(){
         
     <div className="bg-dark">
 
-    <div class="container col-xl-10 col-xxl-8  py-2">
-    <div class="row align-items-center g-lg-5 py-5">
-      <div class="col-lg-5 text-center text-lg-start" id="hiperquimica-animatics">
-        <h2 class="display-4 fw-bold lh-1 text-light mb-3">2D ANIMATIC</h2>
-        <p class="col-lg-10 fs-4 text-light animatics-texto-hiper">Roteiros são transformados em cenas animadas com ilustrações originais que capturam a essência de cada narrativa. Roteiros que ganham vida quadro a quadro</p>
+    <div className="container col-xl-10 col-xxl-8  py-2">
+    <div className="row align-items-center g-lg-5 py-5">
+      <div className="col-lg-5 text-center text-lg-start" id="hiperquimica-animatics">
+        <h2 className="display-4 fw-bold lh-1 text-light mb-3">2D ANIMATIC</h2>
+        <p className="col-lg-10 fs-4 text-light animatics-texto-hiper">Roteiros são transformados em cenas animadas com ilustrações originais que capturam a essência de cada narrativa. Roteiros que ganham vida quadro a quadro</p>
        
         <Link to="/animatics" className="text-light">Clique aqui &nbsp; 
         <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7"/></svg>
