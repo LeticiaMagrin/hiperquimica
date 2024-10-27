01882efd10e92ebe0b9fce1290eb40a2212781c4
Leticia Magrin <leticiamagrin@outlook.com>
2024-10-27 18:13:29 -0300

    criação pagina animatics

--- src/components/header/body/Animatics2.js
+++ src/components/header/body/Animatics2.js
@@ -1,6 +1,6 @@
 import { videos, videos2 } from "../../../Media/Media"
 import VidCarrossel from "./VidCarrossel"
-import { Link } from "react-router-dom";
+
 
 //video Player
 import { DefaultPlayer as Video} from 'react-html5video';
@@ -23,40 +23,50 @@ function Animatics2(){
     <div className="bg-dark">
 
     <div class="container col-xl-10 col-xxl-8  py-2">
-    <div class="row align-items-center g-lg-5 py-5">
-      <div class="col-lg-5 text-center text-lg-start" id="hiperquimica-animatics">
-        <h2 class="display-4 fw-bold lh-1 text-light mb-3">2D ANIMATIC</h2>
-        <p class="col-lg-10 fs-4 text-light animatics-texto-hiper">Roteiros são transformados em cenas animadas com ilustrações originais que capturam a essência de cada narrativa. Roteiros que ganham vida quadro a quadro</p>
-       
-        <Link to="/animatics" className="text-light">Clique aqui &nbsp; 
-        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7"/></svg>
-
-        </Link>
+    <div class="row align-items-center g-lg-12 py-5">
+      <div class="col-lg-12 text-center text-lg-start" id="hiperquimica-animatics">
+        <h2 class="display-4 fw-bold lh-1 text-light mb-3"> 2DAnimatics</h2>
+        <p class="col-lg-10 fs-4 text-light animatics-texto-hiper">A HIPERQUIMICA é conhecida no mercado principalmente pela capacidade de contar histórias em imagens. Nossa expertise sempre foi entender os mecanismos que fazem um roteiro funcionar. É aí que entram nossos craques em imagem e storytelling para escolher, montar e até mesmo suprimir imagens, fazendo com que cada cena dos storyboards e animatics atue para que o filme seja montado em sua potencialidade máxima.</p>
+        <p class="col-lg-10 fs-4 text-light animatics-texto-hiper">Com nossas soluções em animatics, ajudamos grandes marcas a visualizar e ajustar suas campanhas antes da produção final, economizando tempo e recursos enquanto garantimos um produto final alinhado com o público-alvo. A HIPERQUIMICA tem atuado para elevar o padrão da produção audio-visual, entregando historias memoráveis.
+          
+        </p>
       </div>
-      <div className="col-12 col-sm-8 col-lg-7 ">
+
+      </div>
+      <div class="row align-items-center g-lg-12 py-2">
+
+      <div className="col-12 col-sm-8 col-lg-6 ">
       <div className="embed-responsive embed-responsive-16by9 ">
       <div className='vid'>
       
       <div className='video-container pt-4'>
-        <Video
-        
-        >
-          <source src={Claro_Narramatic_Linha2_Ref2} type='video/webm'/>
-        </Video>
-
-      </div>
+     
     </div>
-      {/* <iframe
-      src={Claro_Narramatic_Linha2_Ref2}
-      allowfullscreen
-      style={{ width: '120%', height: '400px' }}
-      /> */}
+  
         </div> 
+      </div>
       
         
       </div>
       <VidCarrossel videos={videos2}/>
     </div>
+    <div class="row align-items-center g-lg-12 py-2">
+
+<div className="col-12 col-sm-8 col-lg-6 ">
+<div className="embed-responsive embed-responsive-16by9 ">
+<div className='vid'>
+
+<div className='video-container pt-3'>
+
+</div>
+
+  </div> 
+</div>
+
+  
+</div>
+<VidCarrossel videos={videos2}/>
+</div>
   </div>
     </div>
         </>
