f06d03c772510fb5c9c766719fb3521c907ff39a
Leticia Magrin <leticiamagrin@outlook.com>
2025-02-06 20:20:59 -0300

    versão final

--- src/components/sobre/Gui.js
+++ src/components/sobre/Gui.js
@@ -9,17 +9,25 @@ function Guilherme(){
 
     return(
         
-        <div class="card mb-3 mx-5 py-3" >
+        <div class="card mb-3 mx-lg-5 mx-auto py-3 mt-2" >
         <div class="row no-gutters">
+        
+              <h5 class="card-title pt-3 text-center ">Gui Fonseca</h5>
+          <div className="col-lg-4 col-11 mt-3 mt-lg-5 px-5 pg-lg-0">
+              <img className='rounded' width="300" height="370" src={Guilherme}/>
+          </div>
+            
+          <div class="col-lg-8 col-sm-12  text-center">
           
-              <h5 class="card-title pt-3 text-center">Gui Fonseca</h5>
-          <div class="col-12 text-center">
-                <div class="card-body">
+          
+            
+          
+                <div class="card-body text-lg-start text-center">
                 <p class="lead mb-4 text-quem-somos">Guilherme Fonseca é um contador de histórias versátil, cuja expertise abrange múltiplas formas de narrativa, incluindo filmes, animações, quadrinhos e webséries. Pós-graduado em Roteiro para Cinema e Televisão pela FAAP e com um MBA em Inteligência Artificial, Guilherme também é formado em Letras, ele combina palavras e imagens com maestria. Com três décadas de experiência na criação de animações e histórias para o público jovem, Guilherme é especialista em storyboards, tendo trabalhado em séries de streaming como “DNA DO CRIME” (Netflix), “O JOGO QUE MUDOU A HISTÓRIA” (Globoplay)  e “AMERICANA” (Star+). No cinema, produziu storyboards para cinco longas-metragens.</p>
                 <p class="lead mb-4 text-quem-somos">Entre suas premiações estão o PROAC de 2017 com a adaptação de "O CRIME DO RESTAURANTE CHINÊS" e o PROAC de 2008 com "ESTAÇÃO LUZ". Ele também é autor de “AUTOMATIC LOVER” (2012), explorando a dualidade entre máquinas e sentimentos.</p>
                 <p class="lead mb-4 text-quem-somos">Sua trajetória reflete um compromisso com a inovação e a versatilidade, transformando cada projeto em uma nova oportunidade de cruzar fronteiras criativas e conectar diferentes formas de arte em narrativas únicas.</p>
-                <p class="lead mb-4 text-quem-somos">www.guifonseca333.com</p>
                 </div>
+                <p class="lead mb-4 text-quem-somos text-center">www.guifonseca333.com</p>
             <div class="card-body">
               <div class="col-10 ">
     
--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,58 +1,88 @@
 import { useState } from "react"
-
 import "./Form.css"
 
-function Form(){
+function Form() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
+  async function sendEmail(e ) {
+    e.preventDefault()
 
-    function sendEmail(e){
-        e.preventDefault();
-
-        if(name === '' || email === '' || message === ''){
-            alert("Por favor, preencha todos os campos");
-            return;
-        }
-        alert("TESTE")
+    if (name === "" || email === "" || message === "") {
+      alert("Por favor, preencha todos os campos")
+      return
     }
 
-    return(
-        <>
+    const data = {
+      sender: { name: name, email: email },
+      to: [{ email: "contato@hiperquimica.com", name: "Hiperquímica" }],
+      subject: `Contato de ${name} através do site `,
+      htmlContent: `
+                <html>
+                    <body>
+                        <h1>Nova mensagem de contato</h1>
+                        <p><strong>Nome:</strong> ${name}</p>
+                        <p><strong>Email:</strong> ${email}</p>
+                        <p><strong>Mensagem:</strong> ${message}</p>
+                    </body>
+                </html>
+            `,
+      replyTo: { email: email, name: name },
+    }
+
+    try {
+      const response = await fetch("https://api.sendinblue.com/v3/smtp/email", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "api-key": "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",
+        },
+        body: JSON.stringify(data),
+      })
+
+      if (response.ok) {
+        alert("Mensagem enviada com sucesso!")
+        setName("")
+        setEmail("")
+        setMessage("")
+      } else {
+        throw new Error("Falha ao enviar a mensagem")
+      }
+    } catch (error) {
+      alert("Erro ao enviar a mensagem: " + (error).message)
+    }
+  }
+
+  return (
     <div className="cont">
       <h1 className="title">Contato</h1>
-
-      <form className="form" onSubmit={() => {}}>
-        <input 
+      <form className="form" onSubmit={sendEmail}>
+        <input
           className="input"
           type="text"
           placeholder="Digite seu nome"
           onChange={(e) => setName(e.target.value)}
           value={name}
         />
-        
-        <input 
+        <input
           className="input"
-          type="text"
+          type="email"
           placeholder="Digite seu email"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
-
-        <textarea 
+        <textarea
           className="textarea"
           placeholder="Digite sua mensagem..."
           onChange={(e) => setMessage(e.target.value)}
           value={message}
         />
-
         <input className="button" type="submit" value="Enviar" />
       </form>
-
     </div>
-        </>
-    )
+  )
 }
 
 export default Form
