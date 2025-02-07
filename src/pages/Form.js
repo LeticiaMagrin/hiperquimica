abf921fd93b5cd33cfb46bf10c73f29fd8a535f5
Leticia Magrin <leticiamagrin@outlook.com>
2025-02-06 21:16:04 -0300

    n aguento mais

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,84 +1,41 @@
-import { useState } from "react"
-import "./Form.css"
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import "./Form.css";
 
 function Form() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
-  async function sendEmail(e ) {
-    e.preventDefault()
 
-    if (name === "" || email === "" || message === "") {
-      alert("Por favor, preencha todos os campos")
-      return
-    }
-
-    const data = {
-      sender: { name: name, email: email },
-      to: [{ email: "", name: "Hiperquímica" }],
-      subject: `Contato de ${name} através do site `,
-      htmlContent: `
-                <html>
-                    <body>
-                        <h1>Nova mensagem de contato</h1>
-                        <p><strong>Nome:</strong> ${name}</p>
-                        <p><strong>Email:</strong> ${email}</p>
-                        <p><strong>Mensagem:</strong> ${message}</p>
-                    </body>
-                </html>
-            `,
-      replyTo: { email: email, name: name },
-    }
-
-    try {
-      const response = await fetch("https://api.sendinblue.com/v3/smtp/email", {
-        method: "POST",
-        
-        body: JSON.stringify(data),
-      })
-
-      if (response.ok) {
-        alert("Mensagem enviada com sucesso!")
-        setName("")
-        setEmail("")
-        setMessage("")
-      } else {
-        throw new Error("Falha ao enviar a mensagem")
-      }
-    } catch (error) {
-      alert("Erro ao enviar a mensagem: " + (error).message)
-    }
-  }
-
-  return (
-    <div className="cont">
-      <h1 className="title">Contato</h1>
-      <form className="form" onSubmit={sendEmail}>
-        <input
-          className="input"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <input
-          className="input"
-          type="email"
-          placeholder="Digite seu email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <textarea
-          className="textarea"
-          placeholder="Digite sua mensagem..."
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-        <input className="button" type="submit" value="Enviar" />
-      </form>
-    </div>
-  )
+    return (
+        <div className="cont">
+            <h1 className="title">Contato</h1>
+            <form className="form" onSubmit={sendEmail}>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder="Digite seu nome"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+                <input
+                    className="input"
+                    type="email"
+                    placeholder="Digite seu email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <textarea
+                    className="textarea"
+                    placeholder="Digite sua mensagem..."
+                    onChange={(e) => setMessage(e.target.value)}
+                    value={message}
+                />
+                <input className="button" type="submit" value="Enviar" />
+            </form>
+        </div>
+    );
 }
 
-export default Form
+export default Form;
