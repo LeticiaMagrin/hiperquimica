3c7076e234b0dae96e22903f04d6d8bea0cd8066
Leticia F. Magrin <108889811+LeticiaMagrin@users.noreply.github.com>
2025-02-09 13:11:20 -0300

    Update quemSomos.js

--- src/pages/quemSomos.js
+++ src/pages/quemSomos.js
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import Headers from "../components/header/header/Headers";
 import Footer from "../components/header/footer/Footer";
 import Guilherme from "../components/sobre/Gui";
 import Mauro from "../components/sobre/Mauro";
 
 function QuemSomos(){
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    
     return(
         <>
         <Headers/>
